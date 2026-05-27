f88e9b8cb8139fadfc452a5ddb637741c694399a
Kacper Sienkiewicz <kacpersienkiewicz@gmail.com>
2026-05-27 18:46:29 -0500

    Created two functions to handle shopping

--- town.py
+++ town.py
@@ -23,83 +23,15 @@ def rest_inn(player):
 
 def go_to_the_market(player):
     """Allows the player to buy new armor or weapons from the store. It should add the bought item to their inventory and allow them to equip it."""
-    left_shop = False # Remove if left_shop is deprecated
     choice = input("You go to the general store. Ingvar has a variety of weapons, and armor for sale. What would you like to buy:\n\t1. Weapons\n\t2. Armor\n\t3. Leave the shop\n")
     while True:
         if choice == '1':
-            # I think this can be put into a function so it can be reused for the weapon and apparel branches.
-            print(f"Index\tName\t\tDamage\tDamage Type\tPrice")
-            for index, value in enumerate(weapons.purchasable_weapons):
-                print(f"{index}\t{value.name}\t{value.damage}\t{value.damage_type}\t{value.value}")
-            while True:
-                choice = input(f"Please enter the Index value of the item you want to buy. Enter '-1' to browse another set of items.\n")
-                if choice == '-1':
-                    left_shop = True # Remove if left_shop is deprecated
-                    break
-                else:
-                    try:
-                        weapon_index = int(choice)
-                        if weapon_index < len(weapons.purchasable_weapons):
-                            purchased_weapon =  weapons.purchasable_weapons[weapon_index]
-                            if player.coins >= purchased_weapon.value:
-                                player.coins -= purchased_weapon.value
-                                player.add_to_inventory(purchased_weapon)
-                                while True:
-                                    choice = input(f"You spend {purchased_weapon.value} coins to purchase the {purchased_weapon.name}. Would you like to equip it? [y/n]?\n")
-                                    if choice =='y':
-                                        player.equip_weapon(purchased_weapon)
-                                        left_shop = True # Necessary to not trigger the else statement in an infinite loop (else: print(f"{choice} is not a valid choice. Please type in 1, 2, or 3.") continue  )
-                                        break
-                                    elif choice == 'n':
-                                        left_shop = True # Remove if left_shop is deprecated
-                                        break
-                                    else:
-                                        print("Only 'y' or 'n' are valid inputs. Please enter one of those.")
-                                break
-                            else:
-                                print(f"You cannot afford the {purchased_weapon.name} as you only have {player.coins} and it costs {purchased_weapon.value} coins.")
-                        else:
-                            print("Please enter a valid index.")                       
-                    except ValueError:
-                        print("Please enter a valid index.")
+            store_logic(player, weapons.purchasable_weapons)
+            break
         elif choice == '2':
-            # I think this can be put into a function so it can be reused for the weapon and apparel branches.
-            print(f"Index\tName\t\tArmor\tPrice")
-            for key, value in enumerate(apparel.purchasable_apparel):
-                print(f"{key}\t{value.name}\t{value.armor}\t{value.value}")
-            while True:
-                choice = input(f"Please enter the Index value of the item you want to buy. Enter '-1' to browse another set of items.\n")
-                if choice == '-1':
-                    left_shop = True # Remove if left_shop is deprecated
-                    break
-                else:
-                    try:
-                        apparel_index = int(choice)
-                        if apparel_index < len(apparel.purchasable_apparel):
-                            purchased_apparel =  apparel.purchasable_apparel[apparel_index]
-                            if player.coins >= purchased_apparel.value:
-                                player.coins -= purchased_apparel.value
-                                player.add_to_inventory(purchased_apparel)
-                                while True:
-                                    choice = input(f"You spend {purchased_apparel.value} coins to purchase the {purchased_apparel.name}. Would you like to equip it? [y/n]?\n")
-                                    if choice =='y':
-                                        player.equip_apparel(purchased_apparel)
-                                        left_shop = True # Necessary to not trigger the else statement in an infinite loop (else: print(f"{choice} is not a valid choice. Please type in 1, 2, or 3.") continue  )
-                                        break
-                                    elif choice == 'n':
-                                        left_shop = True # Remove if left_shop is deprecated
-                                        break
-                                    else:
-                                        print("Only 'y' or 'n' are valid inputs. Please enter one of those.")
-                                break
-                            else:
-                                print(f"You cannot afford the {purchased_apparel.name} as you only have {player.coins} and it costs {purchased_apparel.value} coins.")
-                        else:
-                            print("Please enter a valid index.")                       
-                    except ValueError:
-                        print("Please enter a valid index.")
-
-        elif choice =='3' or left_shop == True:
+            store_logic(player, apparel.purchasable_apparel)
+            break
+        elif choice =='3':
             break
         else:
             print(f"{choice} is not a valid choice. Please type in 1, 2, or 3.")           
@@ -135,3 +67,58 @@ def inventory_management(player):
         else:
             print("Please enter a valid index.")
             continue
+
+def equip_item(player, item):
+    """Equip an equippable item."""
+    if isinstance(item, weapons.Weapon):
+        player.equip_weapon(item)
+    elif isinstance(item, apparel.Apparel):
+        player.equip_apparel(item)
+    else:
+        print(f"Cannot equip item of type {type(item)}")
+    return
+
+def present_store_stock(stock):
+    """Prints out the store's stock in a tabular fashion"""
+    if isinstance(stock[0], weapons.Weapon):
+        print(f"Index\tName\t\tDamage\tDamage Type\tPrice")
+        for index, value in enumerate(stock):
+            print(f"{index}\t{value.name}\t{value.damage}\t{value.damage_type}\t{value.value}")
+    elif isinstance(stock[0], apparel.Apparel):
+        print(f"Index\tName\t\tArmor\tPrice")
+        for key, value in enumerate(apparel.purchasable_apparel):
+            print(f"{key}\t{value.name}\t{value.armor}\t{value.value}")
+    else:
+        print(f"{type(stock[0])} is an invalid type for presenting store stock.")
+    return
+
+def store_logic(player, stock):
+    """Takes in a store's stock, the player's info and figures out the logic for the store so the player can buy stuff."""
+    while True:
+        present_store_stock(stock)
+        choice = input(f"Please enter the Index value of the item you want to buy. Enter '-1' to leave the market.\n")
+        try:
+            stock_index = int(choice)
+        except ValueError:
+            print("Please enter a valid index.")
+            continue
+        
+        if stock_index == -1:
+            break
+        elif stock_index < len(stock):
+            purchased_item = stock[stock_index]
+            if player.coins >= purchased_item.value:
+                player.coins -= purchased_item.value
+                player.add_to_inventory(purchased_item)
+                while True:
+                    choice = input(f"You spend {purchased_item.value} coins to purchase the {purchased_item.name}. Would you like to equip it? [y/n]?\n")
+                    if choice == 'y':
+                        equip_item(player, purchased_item)
+                        break
+                    elif choice == 'n':
+                        break
+                    else:
+                        print("Only 'y' or 'n' are valid inputs. Please enter one of those.")
+                        continue
+            else:
+                print(f"You cannot afford the {purchased_item.name} as you only have {player.coins} and it costs {purchased_item.value} coins.")
